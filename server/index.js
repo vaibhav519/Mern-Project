0f425adee7b8aead617eeb3e47623bbed2aee3aa
vaibhav519 <vaibhav3raj@gmail.com>
2023-02-10 20:38:41 +0530

    mongoDB connection

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extented: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extented: true }));
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://vaibhav:12345@cluster0.tiucar2.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`connection established and running on port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
